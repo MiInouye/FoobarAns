9f5bff784b1b53f6db97f0219d8530b9ae7b481a
Micjela H <kenichi.prod@gmail.com>
2022-09-20 13:43:38 -0700

    solved Ion flux

--- IonFluxConverter.py
+++ IonFluxConverter.py
@@ -2,20 +2,20 @@ def converter_checker(top, converter):
     prev_node = -1
     curr_node = top
     node_ct = top #number of nodes for the subtree
-    print("first  node_ct: " +str(node_ct) + " converter " + str(converter) + " curr_node " + str(curr_node))
+    # print("first  node_ct: " +str(node_ct) + " converter " + str(converter) + " curr_node " + str(curr_node))
     if curr_node == converter:
-        print(" curr_node == converter  converter " + str(converter) + " curr_node " + str(curr_node))
+        # print(" curr_node == converter  converter " + str(converter) + " curr_node " + str(curr_node))
         return prev_node
     prev_node = curr_node
     while node_ct > 1:
-        print("node_ct: " +str(node_ct) + " converter " + str(converter) + " curr_node " + str(curr_node))
+        # print("node_ct: " +str(node_ct) + " converter " + str(converter) + " curr_node " + str(curr_node))
         node_ct = node_ct >> 1
         l = curr_node - node_ct - 1
-        print("l: " +str(l))
-        r = node_ct - 1
-        print("r: " +str(r))
+        # print("l: " +str(l))
+        r = curr_node - 1
+        # print("r: " +str(r))
         if converter == l or converter == r:
-            print("converter == l or converter == r  prev node :" + str(prev_node))
+            # print("if converter == l or converter == r  prev node :" + str(prev_node))
             return prev_node
         if converter < l:
             curr_node = l
@@ -34,9 +34,9 @@ def solution(h, q):
     return [converter_checker(maxNode, converter) for converter in q]
 
 def main():
-    # print("ans" + str(solution(5, [19, 14, 28])))
-    # print("correct : 21,15,29")
-    print("ans" + str(solution(3, [7, 3, 5, 1])))
+    print("ans :" + str(solution(5, [19, 14, 28])))
+    print("correct : 21,15,29")
+    print("ans :" + str(solution(3, [7, 3, 5, 1])))
     print("correct : -1,7,6,3")
 if __name__ == "__main__":
     main()
